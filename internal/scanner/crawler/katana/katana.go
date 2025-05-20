5a8e2e50aac45e1b472b4751e29164b4cef1e7df
ayushkr12 <ayushkr91997@gmail.com>
2025-05-21 03:26:00 +0530

    update katana.go

--- internal/scanner/crawler/katana/katana.go
+++ internal/scanner/crawler/katana/katana.go
@@ -6,8 +6,11 @@ import (
 	"github.com/ayushkr12/spike/internal/pkg/utils"
 )
 
-func CrawlHosts(liveHosts []string, Threads int, ParllelismThreads int) ([]string, error) {
+func CrawlHosts(liveHosts []string, Threads int, ParllelismThreads int, Headless bool) ([]string, error) {
 	katanaArgs := []string{"-s", "-jc", "-kf", "-fx", "-xhr", "-jsl", "-aff",
 		"-t", strconv.Itoa(Threads), "-p", strconv.Itoa(ParllelismThreads)}
+	if Headless {
+		katanaArgs = append(katanaArgs, "-headless")
+	}
 	return utils.RunCommandWithStdinInput("katana", katanaArgs, liveHosts)
 }
